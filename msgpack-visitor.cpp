82863de1c5d9e79a6ad7c82f72dd0f71d27ea43f
Tony Xiang <tony.xiang@alliander.com>
2023-10-18 14:23:38 +0200

    create random
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- msgpack-visitor.cpp
+++ msgpack-visitor.cpp
@@ -5,6 +5,7 @@
 #include <msgpack.hpp>
 #include <cstdint>
 #include <cstddef>
+#include <random>
 
 
 #ifdef _NDEBUG
@@ -13,9 +14,20 @@ constexpr uint32_t N = 10'000'000;
 constexpr uint32_t N = 1'000;
 #endif
 
+void create_msgpack(msgpack::sbuffer& buffer) {
+    msgpack::packer<msgpack::sbuffer> packer(buffer);
+    packer.pack_array(N);
+    std::mt19937 gen(0);
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    for (uint32_t i = 0; i != N; ++i) {
+        packer.pack_double(dis(gen));
+    }
+}
 
 int main()
 {
     std::cout << "Hello CMake." << std::endl;
+    msgpack::sbuffer buffer;
+    create_msgpack(buffer);
     return 0;
 }
